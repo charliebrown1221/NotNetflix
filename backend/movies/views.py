5deb2d25b34cf184fa33ff5cedd1f1b021318192
charliebrown1221 <chaimbrown1@yahoo.com>
2022-07-14 13:11:37 -0400

    added put request

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -29,3 +29,17 @@ def add_movie(request):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view([ 'PUT'])
+@permission_classes([IsAuthenticated])
+def edit_movie(request, pk):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    movie=get_object_or_404(Movies,pk=pk,)
+    if request.method == 'PUT':
+         serializer= CommentSerializer(movie,data=request.data)
+         if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
