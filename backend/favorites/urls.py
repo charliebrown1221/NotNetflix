ab469448cc8e5068c2a0886fd4d914aefa203200
charliebrown1221 <chaimbrown1@yahoo.com>
2022-07-15 16:45:48 -0400

    finished Backend favorite models not needed manytomany in movie models restructered get and post. post takes in movie id , tested in postman

--- backend/favorites/urls.py
+++ backend/favorites/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('<int:user_id>/', views.get_all_favorite_movie),
-    path('add/<int:user_id>', views.add_favorite_movie), 
+    path('all/', views.get_all_favorite_movie),
+    path('add/<int:movie_id>', views.add_favorite_movie), 
     
     ]
