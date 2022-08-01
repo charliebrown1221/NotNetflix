113a27a9153bd6f3a298356f68833641c9b8370b
charliebrown1221 <chaimbrown1@yahoo.com>
2022-08-01 11:08:52 -0400

    added tmdb logo

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import "./Footer.css";
-
+import logo from './assets/long-tmdb.svg'
 const Footer = () => {
   return (
     <footer>
+     <img className="tmdb" src={logo} height="50%" width="20%" ></img>
       <p>Copyright © 2022</p>{" "}
     </footer>
   );
